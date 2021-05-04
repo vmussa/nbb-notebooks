5a7d86a9e86479e09c981f081c858e170a627ec9
Vitor Mussa <vtrmussa@gmail.com>
2021-05-04 12:44:49 -0300

    Fix cleaner and add positions_df join

--- cleaner/cleaner.py
+++ cleaner/cleaner.py
@@ -1,7 +1,4 @@
-# %% Código para a limpeza inicial dos dados raspados do site do NBB
-
-# %% configurações iniciais
-# importa as bibliotecas
+"""Código para a limpeza inicial dos dados raspados do site do NBB."""
 import pandas as pd
 import numpy as np
 import os
@@ -12,17 +9,16 @@ path = "../scraper/output/"
 filenames = os.listdir(path)
 dfs = []
 for filename in filenames:
-    if filename.endswith(".csv"):
+    if filename.endswith(".csv") and filename != "posicoes.csv":
         df = pd.read_csv(path+filename)
         dfs.append(df)
 
-# %% unifica os DataFrames
 # remove coluna pos (info redundante)
-for i in range(len(dfs)):
-    dfs[i] = dfs[i].drop('pos', axis=1)
+for i, _ in enumerate(dfs):
+    dfs[i] = dfs[i].drop('Pos.', axis=1)
 
-# loop com merge em cada df da lista: último df é a tabela unfiicada
-for i in range(len(dfs)):
+# loop com merge em cada df da lista: último df é a tabela unificada
+for i, _ in enumerate(dfs):
     try:
         dfs[i+1] = pd.merge(dfs[i], dfs[i+1])
     except IndexError:
@@ -30,24 +26,30 @@ for i in range(len(dfs)):
 
 all_data = dfs[-1]
 
-# %% separa em duas colunas nomes dos jogadores e números das camisas
+# adiciona posição dos jogadores
+positions_df = pd.read_csv(path+"posicoes.csv")
+all_data = pd.merge(
+    all_data, positions_df, how='left', on=['Jogador', 'Equipe']
+    )
+
+# separa em duas colunas nomes dos jogadores e números das camisas
 # cria coluna nova com o número das camisas
-camisas = all_data.jogador.str.extract(r'(?P<camisa>#\d\d?)')
+camisas = all_data['Jogador'].str.extract(r'(?P<camisa>#\d\d?)')
 all_data['camisa'] = camisas['camisa']
 
 # remove número das camisas da coluna jogador
-all_data.jogador = all_data.jogador.str.replace(
+all_data['Jogador'] = all_data['Jogador'].str.replace(
     re.compile(r"\s#\d\d?"),
     ''
     )
 
-# %% altera a ordem das colunas para uma melhor leitura dos dados
+# altera a ordem das colunas para uma melhor leitura dos dados
 
-# %% verifica inconsistências na entrada de dados
+# verifica inconsistências na entrada de dados
 # colunas categóricas
 
 # colunas numéricas
 
-# %% exporta tabela unificada para csv e excel
+# exporta tabela unificada para csv e excel
 all_data.to_csv('output/nbb_estatisticas.csv', index=False)
 all_data.to_excel('output/nbb_estatisticas.xlsx', index=False)
